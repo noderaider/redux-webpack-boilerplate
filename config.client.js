14ba01265020860194ab57ea4bb70b8cd842918f
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-04-15 21:17:16 -0700

    built

--- config.client.js
+++ config.client.js
@@ -3,11 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
                                                                                         value: true
 });
-<<<<<<< HEAD
-exports.createClientLogger = exports.baseUrl = exports.appKey = exports.client = exports.environment = undefined;
-=======
 exports.createClientLogger = exports.baseUrl = exports.port = exports.hostname = exports.appName = exports.environment = exports.appKey = exports.client = undefined;
->>>>>>> f3d34b88cebdc51dfc0f9d05fa8306b9c36478e2
 
 var _configClient = require('./config-client');
 
@@ -21,11 +17,7 @@ var _name2 = _interopRequireDefault(_name);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-<<<<<<< HEAD
 var appKey = _name2.default.replace(/-/g, '_');
-var environment = _configClient2.default.environment;
-var hostname = _configClient2.default.hostname;
-=======
 var environment = _configClient2.default.environment;
 var appName = _configClient2.default.appName;
 var hostname = _configClient2.default.hostname;
@@ -33,29 +25,16 @@ var port = _configClient2.default.port;
 
 
 var baseUrl = 'http' + (environment === 'debug' ? '' : 's') + '://' + hostname;
->>>>>>> f3d34b88cebdc51dfc0f9d05fa8306b9c36478e2
-
-var baseUrl = 'http' + (environment === 'debug' ? '' : 's') + '://' + hostname;
 
 var fakeLogger = function fakeLogger(name) {
-<<<<<<< HEAD
-                                                                                        return { name: name + '-fake', trace: function trace() {}, debug: function debug() {}, info: function info() {}, warn: function warn() {}, error: function error() {}, fatal: function fatal() {} };
+                                                                                        return { name: appName + '-fake', trace: function trace() {}, debug: function debug() {}, info: function info() {}, warn: function warn() {}, error: function error() {}, fatal: function fatal() {} };
 };
 var createClientLogger = function createClientLogger(name) {
-                                                                                        return process.env.NODE_ENV !== 'production' ? (0, _bunyan.createLogger)({ name: appKey,
+                                                                                        return process.env.NODE_ENV !== 'production' ? (0, _bunyan.createLogger)({ name: appName,
                                                                                                                                                                                 level: environment === 'debug' ? 'debug' : 'info'
                                                                                         }) : fakeLogger(name);
-=======
-                                                          return { name: appName + '-fake', trace: function trace() {}, debug: function debug() {}, info: function info() {}, warn: function warn() {}, error: function error() {}, fatal: function fatal() {} };
-};
-var createClientLogger = function createClientLogger(name) {
-                                                          return __DEV__ ? (0, _bunyan.createLogger)({ name: appName,
-                                                                                                                    level: 'debug'
-                                                          }) : fakeLogger(name);
->>>>>>> f3d34b88cebdc51dfc0f9d05fa8306b9c36478e2
 };
 
-exports.environment = environment;
 exports.client = _configClient2.default;
 exports.appKey = appKey;
 exports.environment = environment;
