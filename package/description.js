f1d0073916145449c07fed3c74acec95712e3b5f
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-04-29 12:31:52 -0700

    bump

--- package/description.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = 'Generic base project for Redux / React / Webpack / Babel projects and libraries.';
