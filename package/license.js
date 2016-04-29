30ce9956bece8b8ab4bb57cae564708ab1e4c0d0
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-04-29 01:17:38 -0700

    done for day

---
+++ package/license.js
@@ -0,0 +1,11 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+exports.default = function (_ref) {
+  var _ref$license = _ref.license;
+  var license = _ref$license === undefined ? 'MIT' : _ref$license;
+  return license;
+};
